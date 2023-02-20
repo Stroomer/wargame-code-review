5b93a69a23c533d542bbfdb2f1d8d31687887a6e
Lukas Stroomer <lukasstroomer@gmail.com>
2023-02-20 16:28:28 +0100

    add clickable map with pixel-color-check

--- src/assets/js/Map.js
+++ src/assets/js/Map.js
@@ -27,6 +27,26 @@ class Map
         {
             this.territories[i].init();
         }
+
+        this.canvas.addEventListener('click', this.click.bind(this));
+    }
+
+    click(event) 
+    {
+        const canvas = event.target;
+        const ctx    = canvas.getContext('2d');
+        const rect   = canvas.getBoundingClientRect();
+        const x      = event.clientX - rect.left;
+        const y      = event.clientY - rect.top;
+        const width  = rect.width;
+        const height = rect.height;
+        const scaleX = width / 320;
+        const scaleY = height / 180;
+        const data   = ctx.getImageData(x/scaleX, y/scaleY, 1, 1).data;
+        const rgb    = ((data[0] << 16) | (data[1] << 8) | (data[2])).toString(16);
+        const hex    = "#" + ("00000000" + rgb).slice(-6);
+
+        console.log( hex ); 
     }
 
     draw(buffer, canvas)
@@ -40,3 +60,18 @@ class Map
 }
 
 export default Map;
+
+
+
+// function GetPixel(context, x, y)
+// {
+//     var p = context.getImageData(x, y, 1, 1).data; 
+//     var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);  
+//     return hex;
+// }
+
+// function rgbToHex(r, g, b) {
+//     if (r > 255 || g > 255 || b > 255)
+//         throw "Invalid color component";
+//     return ((r << 16) | (g << 8) | b).toString(16);
+// }
